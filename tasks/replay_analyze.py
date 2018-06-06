f0954040f0321a18f2b656fd03b48c4b481dc182
Sunpy <sunpy@osufx.com>
2018-06-06 10:21:37 +0200

    Analyze object structure

--- tasks/replay_analyze.py
+++ tasks/replay_analyze.py
@@ -2,14 +2,23 @@ import base64
 from objects.replay import Replay
 
 class Analyze(object):
+	score_id	= 0
+	beatmap_id	= 0
+	user_id		= 0
+	game_mode	= 0
+	pp			= 0
+	replay 		= None
+	
 	def __init__(self, data):
-		self.data = data
-		self.ParseReplay()
+		# Put data types into self
+		for key, value in data.items():
+			if key is not "replay_data":
+				setattr(self, key, value)
+
+		self.ParseReplay(data["replay_data"])
 
 		#TODO: Do the analyze stuffz
 	
-	def ParseReplay(self):
-		replay_bytes = base64.b64decode(self.data["replay_data"])
-		del(self.data["replay_data"])
-
+	def ParseReplay(self, base64_replay_data):
+		replay_bytes = base64.b64decode(base64_replay_data)
 		self.replay = Replay(replay_bytes)
